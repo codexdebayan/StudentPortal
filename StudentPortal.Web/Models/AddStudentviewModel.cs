5b6f0413016bef575756e2d7f775b366af48210b
HP <cdebayan24@gmail.com>
2024-04-16 23:30:37 +0530

    Add project files.

---
+++ StudentPortal.Web/Models/AddStudentviewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentPortal.Web.Models
+{
+    public class AddStudentviewModel
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public bool Subcribed { get; set; }
+    }
+}
